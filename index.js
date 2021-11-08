10fb1bb2439074ab545be4b8e0f4ebd8184b8c01
MasudRana924 <masud15-924@diu.edu.bd>
2021-11-08 00:23:45 -0600

    user added

--- index.js
+++ index.js
@@ -18,17 +18,14 @@ async function run() {
         await client.connect();
         const database=client.db('doctors')
         const appointmentsCollections=database.collection('appointments')
-        // get all data 
-        // app.get('/appointments', async (req, res) => {
-        //     const cursor =appointmentsCollections.find({})
-        //     const appointments = await cursor.toArray()
-        //     res.send(appointments)
-        // })
+        const usersCollections=database.collection('users')
+    //  post user appointments
         app.post('/appointments',async(req,res)=>{
             const appointment = req.body;
             const result = await appointmentsCollections.insertOne(appointment);
             res.json(result)
         })
+        // get all data through email 
         app.get('/appointments/email', async (req, res) => {
             const email = req.query.email;
             const date = new Date (req.query.date).toLocaleDateString() ;
@@ -38,6 +35,23 @@ async function run() {
             const appointments = await cursor.toArray();
             res.json(appointments);
         })
+
+        // post user 
+        app.post('/users',async(req,res)=>{
+            const user = req.body;
+            const result = await usersCollections.insertOne(user);
+            res.json(result)
+        })
+    //    google signin or checked users
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollections.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
     }
     finally {
         // await client.close();
